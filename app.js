f5fc51e1664f116c80eff67aef7e57abb60e46b5
patrickackerman <patrickackerman@gmail.com>
2015-11-02 12:44:01 -0600

    revised

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ var app = angular.module('friendsList', []);
 app.controller('mainCtrl', function($scope){
     $scope.name = "Patrick Ackerman";
     $scope.friends = [{name: 'Patrick Ackerman', age: '30', birthplace: 'Bethlehem'}, {name: 'Patrick Ackerman', age: '30', birthplace: 'Bethlehem'}, {name: 'Patrick Ackerman', age: '30', birthplace: 'Bethlehem'}, {name: 'Patrick Ackerman', age: '30', birthplace: 'Bethlehem'}];
+    $scope.addFriend = function (newFriend) {
+      $scope.friends.push({name: newFriend, age: '?', birthplace: '?'});
+      $scope.newFriendInput = '';
+    }
 });
